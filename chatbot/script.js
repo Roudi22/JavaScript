e31a9c50aed4aef666f192cb8a44f79ebd5bb349
Roudi22 <roudi.alzkemi@gmail.com>
2023-08-18 21:16:23 +0200

    Handling the outcoming messages from the user

---
+++ chatbot/script.js
@@ -0,0 +1,23 @@
+const chatInput = document.querySelector(".chat-input textarea"); // grab the textarea tag
+const sendChatBtn = document.querySelector(".chat-input span"); // grab the send button
+const chatbox = document.querySelector(".chatbox"); 
+let userMessage;
+
+// create a function to add the user input to the chat box
+const createChatLi = (message, className) => {
+    const chatLi = document.createElement("li"); // create li element and save it in a variable
+    chatLi.classList.add("chat", className); // adding the nesseccery classes to the creted element
+    let chatContent = className === "outgoing" ? `<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</span>` // setting the chat content depending on the class if it's outgoing or sopmething else
+    chatLi.innerHTML = chatContent;
+    return chatLi
+}
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim(); // Getting entered message and removing extra whitespace
+    if(!userMessage) return // Return if the message is empty
+
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatInput.value = ""
+}
+
+sendChatBtn.addEventListener("click", handleChat);
